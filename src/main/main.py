0d35b3037f40308eed8b1debdc5340c700338f01
anjalifernando <anjalifernando@github.com>
2026-07-21 20:17:09 +0000

    Test run: 0/5 passed, 5 failed, 0 skipped - 2026-07-21T20:17:09.603Z

--- src/main/main.py
+++ src/main/main.py
@@ -56,14 +56,15 @@ def load_and_clean_users(file_path):
         next(reader, None)
 
         for row in reader:
-            if len(row) != 3:
+            if len(row) != 2:
                 continue
+
             if any(value.strip() == "" for value in row):
                 continue
 
             cursor.execute(
-                "INSERT INTO users (userId, firstName, lastName) VALUES (?, ?, ?)",
-                (int(row[0]), row[1], row[2])
+                "INSERT INTO users (userId, firstName, lastName) VALUES (?, ?)",
+                (int(row[0]), row[1])
             )
 
     conn.commit()
